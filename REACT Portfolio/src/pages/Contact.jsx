6f3b84ddaccd66f09b41061ad990f60472f742b1
J Mac <jbatcheller123@gmail.com>
2024-03-19 18:30:57 -0600

    Added some more to the contact page, added a form

--- REACT Portfolio/src/pages/Contact.jsx
+++ REACT Portfolio/src/pages/Contact.jsx
@@ -1,20 +1,67 @@
-export default function Contact() {
+import { useState } from 'react';
+
+const styles = {
+    formStyle: {
+        margin: '20px',
+    }, 
+
+    formInputStyle: {
+        display: 'block',
+        marginTop: '5px',
+        marginBottom: '5px',
+        width: '100%,'
+    }
+}
+
+function Contact() {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState(''); 
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target; 
+
+        return name === 'firstName' ? setFirstName(value) : setLastName(value); 
+    }; 
+
+    const handleFormSubmit = (e) => {
+
+        e.preventDefault(); 
+
+        alert(`Hello, ${firstName} ${lastName}`); 
+        setFirstName(''); 
+        setLastName('');
+    }; 
+
+
     return (
-        <div>
-
-            <h1>Contact Me</h1>
-
-            <p>
-                <a href="mailto: jbatcheller123@gmail.com">Email</a>
-                <a href="https://github.com/jbatcheller32">GitHub</a>
-            </p>
 
 
-
-
-
-
-
-        </div>
+        <div className="container text-center">
+      <h1>
+        Hello {firstName} {lastName}
+      </h1>
+      <form style={styles.formStyle} className="form" onSubmit={handleFormSubmit}>
+        <input style={styles.formInputStyle}
+          value={firstName}
+          name="firstName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="First Name"
+        />
+        <input style={styles.formInputStyle} 
+          value={lastName}
+          name="lastName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Last Name"
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
     )
 }
+
+export default Contact;
--- REACT Portfolio/src/pages/Portfolio.jsx
+++ REACT Portfolio/src/pages/Portfolio.jsx
@@ -1,9 +1,16 @@
+import Project from '../components/Project';
+
 export default function Portfolio() {
     return (
 
         <div>
 
             <h1>Portfolio</h1>
+        <main> 
+            <Project />
+
+        </main>
+            
 
             
 
