eab3e2d36d50ed9ef89cade3bdecf9292f69f91f
J Mac <jbatcheller123@gmail.com>
2024-03-27 13:07:01 -0600

    adding some styling to all my pages to make this look more professional

--- REACT Portfolio/src/pages/About.jsx
+++ REACT Portfolio/src/pages/About.jsx
@@ -10,19 +10,20 @@ export default function About() {
             <p>
             With over a decade of experience in customer-focused roles across various industries, I bring a wealth of expertise to the tech field as a Technical Support Specialist. 
             Armed with an AAS in Computer Information Systems and currently pursuing further knowledge through a full stack coding bootcamp at Denver University, 
-            I am deeply committed to advancing my skills and contributing meaningfully to the ever-evolving landscape of technology.
+            I am deeply committed to advancing my skills and contributing meaningfully to the ever-evolving landscape of technology. <br />
 
             My tenure as a Technical Support Specialist at IDX Broker LLC honed my ability to provide comprehensive user education, facilitate seamless client onboarding, and drive customer success. 
-            I am adept at aligning decisions with both personal and organizational objectives, ensuring that every action I take contributes to the attainment of overarching goals.
-
+            I am adept at aligning decisions with both personal and organizational objectives, ensuring that every action I take contributes to the attainment of overarching goals.<br />
+            <br />
             As an Apple Certified Technician at Simply Mac, I demonstrated proficiency in troubleshooting and diagnosing complex hardware and software issues, coupled with the ability to provide insightful guidance to customers navigating challenges with iOS and macOS devices. 
-            My meticulous approach to repair work ensured the swift and accurate restoration of Apple products to optimal functionality.
-
+            My meticulous approach to repair work ensured the swift and accurate restoration of Apple products to optimal functionality.<br />
+            <br />
             In addition to my technical prowess, my experience extends to roles such as Pro Shop Assistant/ Marshal Staff Manager at RiverRidge Golf and Events, where I orchestrated diverse events and provided comprehensive training to staff, 
-            emphasizing the importance of adherence to safety protocols and exceptional guest service. Similarly, my tenure as a Table Games Dealer at Lady Luck Casino Hotel equipped me with keen observational skills and a commitment to upholding integrity and fair play.
-
+            emphasizing the importance of adherence to safety protocols and exceptional guest service. Similarly, my tenure as a Table Games Dealer at Lady Luck Casino Hotel equipped me with keen observational skills and a commitment to upholding integrity and fair play.<br />
+            <br />
             Driven by a passion for technology and a dedication to delivering excellence in customer service, I am poised to leverage my diverse skill set and industry knowledge to make valuable contributions in the tech field. 
-            I thrive in dynamic environments where innovation is paramount and am eager to collaborate with like-minded professionals to drive organizational success and technological advancement.
+            I thrive in dynamic environments where innovation is paramount and am eager to collaborate with like-minded professionals to drive organizational success and technological advancement.<br />
+            <br />
             </p>
 
 
